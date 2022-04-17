0b63cd3d1ac2ae28d3f561cefe4ac26ea0261ff6
rctrinity <upfront-boxing-0j@icloud.com>
2022-04-17 15:04:52 -0500

    Added Bitcoin

--- pkg/__init__.py
+++ pkg/__init__.py
@@ -1,2 +1,3 @@
 GoldForexURL = "https://forex-data-feed.swissquote.com/public-quotes/bboquotes/instrument/XAU/USD"
 SilverForexURL = "https://forex-data-feed.swissquote.com/public-quotes/bboquotes/instrument/XAG/USD"
+BtcURL = "https://api.coindesk.com/v1/bpi/currentprice.json"
