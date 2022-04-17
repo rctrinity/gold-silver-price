5980cfe91898b7dfc654ff23b4f5468caf046fac
rctrinity <upfront-boxing-0j@icloud.com>
2022-04-17 17:50:03 -0500

    Added Bitcoin

--- goldsilverprice.py
+++ goldsilverprice.py
@@ -4,16 +4,19 @@
 if (__name__ == '__main__'):
     
     import pkg
+    from pkg import *
     import json, requests
     import datetime
     import logging
     
     now = datetime.datetime.now()
-    FORMAT = '[%(asctime)-15s] - [%(levelname)s] - %(message)s'
-    logging.basicConfig(format=FORMAT, level=logging.INFO, datefmt='%Y-%m-%d %H:%M:%S')
     logger = logging.getLogger(__name__)
+    logger.setLevel('INFO')
+    stream_handler = logging.StreamHandler()
+    stream_handler.setFormatter(ColorLogFormatter())
+    logger.addHandler(stream_handler)
 
-    logger.info('Collecting data from Forex...')
+    logger.info('Collecting data from Forex...', extra={'prefix': Color.GREEN, 'suffix': Color.END})
 
     #gold
     url = requests.get(pkg.GoldForexURL)
@@ -22,9 +25,9 @@ if (__name__ == '__main__'):
     try:
         for k in forex:
 	        if k['topo']['platform'] == 'MT5':
-		        logger.info('GOLD    : $'+format(k['spreadProfilePrices'][0]['ask'], ',.2f'))
+		        logger.info('GOLD    : $'+format(k['spreadProfilePrices'][0]['ask'], ',.2f'), extra={'prefix': Color.YELLOW, 'suffix': Color.END})
     except:
-        logger.info('Unable to retrieve GOLD price')
+        logger.info('Unable to retrieve GOLD price', extra={'prefix': Color.RED, 'suffix': Color.END})
 
     #silver
     url = requests.get(pkg.SilverForexURL)
@@ -33,21 +36,21 @@ if (__name__ == '__main__'):
     try:
         for k in forex:
 	        if k['topo']['platform'] == 'MT5':
-		        logger.info('SILVER  :    $'+format(k['spreadProfilePrices'][0]['ask'], ',.2f'))
+		        logger.info('SILVER  :    $'+format(k['spreadProfilePrices'][0]['ask'], ',.2f'), extra={'prefix': Color.SILVER, 'suffix': Color.END})
     except:
-        logger.info('Unable to retrieve SILVER price')
+        logger.info('Unable to retrieve SILVER price', extra={'prefix': Color.RED, 'suffix': Color.END})
 
     #BTC
-    logger.info('Collecting data from CoinDesk...')
+    logger.info('Collecting data from CoinDesk...', extra={'prefix': Color.GREEN, 'suffix': Color.END})
     url = requests.get(pkg.BtcURL)
     btc_json = url.text
     btc = json.loads(btc_json)
     try:
         btc_f = float(btc['bpi']['USD']['rate'].replace(',',''))
         satusd = (1/(btc_f * 0.00000001))
-        logger.info('BITCOIN :$'+format(btc_f, ',.2f'))
-        logger.info('SATs/USD:     '+format(satusd, ',.0f'))
+        logger.info('BITCOIN :$'+format(btc_f, ',.2f'), extra={'prefix': Color.YELLOW, 'suffix': Color.END})
+        logger.info('SATs/USD:     '+format(satusd, ',.0f'), extra={'prefix': Color.SILVER, 'suffix': Color.END})
     except:
-        logger.info('Unable to retrieve BTC price')
+        logger.info('Unable to retrieve BTC price', extra={'prefix': Color.RED, 'suffix': Color.END})
 
 quit()
--- pkg/__init__.py
+++ pkg/__init__.py
@@ -1,3 +1,47 @@
+import logging
+
 GoldForexURL = "https://forex-data-feed.swissquote.com/public-quotes/bboquotes/instrument/XAU/USD"
 SilverForexURL = "https://forex-data-feed.swissquote.com/public-quotes/bboquotes/instrument/XAG/USD"
 BtcURL = "https://api.coindesk.com/v1/bpi/currentprice.json"
+
+class Color:
+    """A class for terminal color codes."""
+
+    BOLD = "\033[1m"
+    BLUE = "\033[94m"
+    WHITE = "\033[97m"
+    GREEN = "\033[92m"
+    YELLOW = "\033[93m"
+    SILVER = "\033[37m"
+    RED = "\033[91m"
+    BOLD_WHITE = BOLD + WHITE
+    BOLD_BLUE = BOLD + BLUE
+    BOLD_GREEN = BOLD + GREEN
+    BOLD_YELLOW = BOLD + YELLOW
+    BOLD_RED = BOLD + RED
+    END = "\033[0m"
+
+
+class ColorLogFormatter(logging.Formatter):
+    """A class for formatting colored logs."""
+
+    FORMAT = '%(prefix)s%(levelname)s - %(asctime)-15s - %(message)s%(suffix)s'
+
+    LOG_LEVEL_COLOR = {
+        "DEBUG": {'prefix': '', 'suffix': ''},
+        "INFO": {'prefix': '', 'suffix': ''},
+        "WARNING": {'prefix': Color.BOLD_YELLOW, 'suffix': Color.END},
+        "ERROR": {'prefix': Color.BOLD_RED, 'suffix': Color.END},
+        "CRITICAL": {'prefix': Color.BOLD_RED, 'suffix': Color.END},
+    }
+
+    def format(self, record):
+        """Format log records with a default prefix and suffix to terminal color codes that corresponds to the log level name."""
+        if not hasattr(record, 'prefix'):
+            record.prefix = self.LOG_LEVEL_COLOR.get(record.levelname.upper()).get('prefix')
+        
+        if not hasattr(record, 'suffix'):
+            record.suffix = self.LOG_LEVEL_COLOR.get(record.levelname.upper()).get('suffix')
+
+        formatter = logging.Formatter(self.FORMAT)
+        return formatter.format(record)
