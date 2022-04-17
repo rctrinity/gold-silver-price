d56609eaca3d708b53623a6f8e6729c8e70d924a
rctrinity <103879453+rctrinity@users.noreply.github.com>
2022-04-17 04:37:28 -0500

    Create __init__.py

---
+++ pkg/__init__.py
@@ -0,0 +1,2 @@
+GoldForexURL = "https://forex-data-feed.swissquote.com/public-quotes/bboquotes/instrument/XAU/USD"
+SilverForexURL = "https://forex-data-feed.swissquote.com/public-quotes/bboquotes/instrument/XAG/USD"
